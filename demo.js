ddd90448992a2240599d90a9591b87e2722a40cc
Havenganesh <ganeshandimadam@gmail.com>
2022-07-02 20:31:29 +0530

    "cimmted 1st"

--- demo.js
+++ demo.js
@@ -1,4 +1,13 @@
 var name = prompt("Enter your name")
 var age = prompt("Enter your age")
 agedays = age * 365.25
-alert(agedays)
+alert("hi"+name+"enjoyed your life "+agedays)
+var number = prompt("Enter any Number")
+if(number%2==0)
+{
+    console.log("even number")
+}
+else
+{
+    console.timeLog("odd number")
+}
---
+++ loopdemo.js
@@ -0,0 +1,16 @@
+var age = prompt("what is your age")
+if(age>=60)
+{
+    console.log("first priority")
+    alert("first")
+}
+else if(age>=40)
+{
+    console.log("second priority")
+    alert("second")
+}
+else
+{
+    console.log("third priority")
+    alert("third")
+}
