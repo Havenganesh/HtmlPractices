b38b1a203f611be3061b5e3c3830b7217f74bfd5
Havenganesh <ganeshandimadam@gmail.com>
2022-07-02 22:28:50 +0530

    "commited"

---
+++ divisordemo.js
@@ -0,0 +1,14 @@
+var number=prompt("enter a number")
+div =2
+while(div<=number)
+{
+    if(number%div==0)
+    {console.log(div)}
+    div++
+}
+var favorite =prompt("who is your favorite actor")
+while(favorite!="yogi babu")
+{
+
+    favorite=prompt("enter another actor name")
+}
